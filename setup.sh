36f24b7b58b987c7bd57cdf042326f97b4d21a9c
Ämilian Mayrhofer <aemilian.mayrhofer@gmail.com>
2022-07-30 16:05:18 +0200

    improved setup script

--- setup.sh
+++ setup.sh
@@ -2,3 +2,13 @@
 python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+
+if [ ! -f ".env" ]; then
+    echo "No .env file found, copied sample.env."
+    cp sample.env .env
+fi
+
+echo "Setup complete."
+echo "To start the API run 'flask run'"
+echo ""
+echo "Please make sure to run this file as source ('source setup.sh') or activate the venv manually ('source venv/bin/activate')"

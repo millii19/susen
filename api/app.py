bb9089a6400b9134ee0c314744d8628a8c040a49
Ämilian Mayrhofer <aemilian.mayrhofer@gmail.com>
2022-07-30 22:17:56 +0200

    added skeleton for simulate endpoint

--- api/app.py
+++ api/app.py
@@ -1,17 +1,25 @@
 import connexion
 import logging
+import os
 
+app = connexion.App(__name__, specification_dir="docs/")
+log_level = (
+    logging.DEBUG if os.environ.get("FLASK_ENV") == "development" else logging.INFO
+)
+logging.basicConfig(level=log_level)
 
 
 def get_status():
-    return {
-        "version": "0.0.1-peta_1.3.44"
-    }
+    return {"version": "0.0.1-peta_1.3.44"}
 
-logging.basicConfig(level=logging.INFO)
-app = connexion.App(__name__, specification_dir='docs/')
+
+def simulate(budget, latitude, longitude):
+    logging.debug(
+        f"Budget: {budget} ({type(budget)}); Latitude: {latitude} ({type(latitude)}); Longitude: {longitude} ({type(longitude)});"
+    )
+
+
 app.add_api("v1.yml")
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=8080)
